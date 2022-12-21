eae60c552930656f02bb5933209f5ca833d94a23
Ryan Wright <117471213+animetechie@users.noreply.github.com>
2022-12-21 13:08:07 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
+# Import the Microsoft Graph API client
+import graph_api_client
+
+# Get the user's OneDrive client
+client = graph_api_client.OneDriveClient()
+
+# Sync the user's OneDrive folders
+client.sync()
+
+# Sign the user out of OneDrive
+client.sign_out()
+
+# Sign the user back in to OneDrive
+client.sign_in()
+
 # Import the OneNote API
 import onenote_api
 
 # Get the user's OneDrive for Business client
 client = onenote_api.OnenoteClient()
 
+# Sync the user's OneNote notes
+client.sync()
+
 # Sign the user out of OneNote
 client.sign_out()
 
 # Sign the user back in to OneNote
 client.sign_in()
-
-# Sync the user's notes
-client.sync()
